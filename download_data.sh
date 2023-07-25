9577139483230274b8fb98d79b5646fcc587d5ca
John Wharington <jwharington@gmail.com>
2023-07-25 23:31:30 +1000

    initial commit

---
+++ download_data.sh
@@ -0,0 +1,3 @@
+wget -O ogn.fln https://ddb.glidernet.org/download/download-fln.php
+wget -O AirspaceAUS.txt "https://xcaustralia.org/download/class_all_NO_fia_&_e%20class%20freq.bnd.php"
+wget -O AirfieldsAUS.cup "https://xcaustralia.org/download/cu_rwy_wpt_info.php"
